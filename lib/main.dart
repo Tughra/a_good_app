f11bc2045de89f9eb326fc1114dc734d8425eece
MuhammedZeyrek <tugrazeyrek44@gmail.com>
2022-08-31 23:46:09 +0300

    task

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+
+import 'package:a_good_app/core/notification_service.dart';
 import 'package:a_good_app/firebase_database_helper.dart';
 import 'package:a_good_app/screen/wrapper.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -5,39 +8,53 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
+import 'package:workmanager/workmanager.dart';
 
 import 'services/auth_service.dart';
 
-void main() async{
+void callbackDispatcher() {
+  Workmanager().executeTask((taskName, inputData) async {
+    if (taskName == "trigger") {
+
+        LocalNotificationService.instance.showNotification(
+            hashcode: 2, payload: "payload"
+            , title: "WorkManager",
+            body: "WorkManager is triggered"
+        );
+      debugPrint("**********-**********");
+    }
+    return Future.value(true);
+  });
+}
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await Workmanager().cancelAll().then((value) {
+    Workmanager().initialize(callbackDispatcher, isInDebugMode: true).then((
+        value) => Workmanager().registerOneOffTask("1", "trigger"));
+  });
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: StreamProvider<User?>.value(initialData:null ,value:AuthService().userChanges,child: const Wrapper()),
+      home: StreamProvider<User?>.value(
+          initialData: null,
+          value: AuthService().userChanges,
+          child: const Wrapper()),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
-
-
-
-
-
-
-
 
   final String title;
 
@@ -47,51 +64,23 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  final FirebaseDataBaseHelper _helper=FirebaseDataBaseHelper();
+  final FirebaseDataBaseHelper _helper = FirebaseDataBaseHelper();
+
   @override
   void _incrementCounter() {
     setState(() {
-
-
-
-
-
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
-
-
-
-
-
     return Scaffold(
       appBar: AppBar(
-
-
         title: Text(widget.title),
       ),
       body: Center(
-
-
         child: Column(
-
-
-
-
-
-
-
-
-
-
-
-
-
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -99,13 +88,16 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .headline4,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           _helper.getFirst();
           print("asdsad");
         },
